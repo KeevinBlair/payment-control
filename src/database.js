7f8e198e6308ae5d63e674d9e0429441eca3fb4a
KeevinBlair <kevinblair317@gmail.com>
2026-08-08 17:50:24 -0300

    test

--- src/database.js
+++ src/database.js
@@ -4,14 +4,14 @@ module.exports = {
   usuarios: [{
     id: '00000000-0000-4000-8000-000000000001',
     email: 'admin@admin.com',
-    senha: '123123',
+    senha: '$2a$10$xtqV3Pqp1ZK.lrKv3i5Piee/mk4ajBGh0MPV/NR7m9GILQErHw5gW',
     nome: 'ADMIN',
     ativo: true
   },
   {
     id: '00000000-0000-4000-8000-000000000002',
     email: 'inativo@admin.com',
-    senha: '123123',
+    senha: '$2a$10$xtqV3Pqp1ZK.lrKv3i5Piee/mk4ajBGh0MPV/NR7m9GILQErHw5gW',
     nome: 'Teste Inativo',
     ativo: false
   }],
